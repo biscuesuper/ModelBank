3c0eb26b96c23dcb7db8788e4165c6b5a62634c4
biscuesuper <ioana.bacrau@gmail.com>
2021-12-27 11:37:43 +0000

    Add projects for ASPSP API and AISP

---
+++ ModelBank/AISP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AISP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
