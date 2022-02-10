7f73c6bdaf26044ed853d55ad50c7fd810f3c297
biscuesuper <ioana.bacrau@gmail.com>
2022-02-10 23:01:22 +0000

    Add content in these classes (not accurate, but at least not empty)

--- ModelBank/OBTemplate/Enumerations/ISO/ExternalBankTransactionFamily1Code.cs
+++ ModelBank/OBTemplate/Enumerations/ISO/ExternalBankTransactionFamily1Code.cs
@@ -2,5 +2,24 @@
 {
     public class ExternalBankTransactionFamily1Code
     {
+        private string _value;
+
+        public ExternalBankTransactionFamily1Code()
+        {
+            _value = "";
+        }
+
+        public ExternalBankTransactionFamily1Code(string value)
+        {
+            this._value = value;
+        }
+        public static implicit operator string(ExternalBankTransactionFamily1Code d)
+        {
+            return d._value;
+        }
+        public static implicit operator ExternalBankTransactionFamily1Code(string d)
+        {
+            return new ExternalBankTransactionFamily1Code(d);
+        }
     }
 }
--- ModelBank/OBTemplate/Enumerations/ISO/Min3Max4Text.cs
+++ ModelBank/OBTemplate/Enumerations/ISO/Min3Max4Text.cs
@@ -2,5 +2,24 @@
 {
     public class Min3Max4Text
     {
+        private string _value;
+
+        public Min3Max4Text()
+        {
+            _value = "";
+        }
+
+        public Min3Max4Text(string value)
+        {
+            this._value = value;
+        }
+        public static implicit operator string(Min3Max4Text d)
+        {
+            return d._value;
+        }
+        public static implicit operator Min3Max4Text(string d)
+        {
+            return new Min3Max4Text(d);
+        }
     }
 }
