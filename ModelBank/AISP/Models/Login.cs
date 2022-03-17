1578d37167ccad115589396b8f4365626cbc8ff4
biscuesuper <ioana.bacrau@gmail.com>
2022-03-17 05:21:20 +0000

    Bug add id to login obj

--- ModelBank/AISP/Models/Login.cs
+++ ModelBank/AISP/Models/Login.cs
@@ -4,6 +4,7 @@ namespace AISP.Models
 {
     public class Login
     {
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
     }
