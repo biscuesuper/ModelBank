2ccd489d832a540593490ca8e43cf339e5b958ee
biscuesuper <ioana.bacrau@gmail.com>
2022-04-06 21:14:51 +0100

    add aisp test data db script

---
+++ SQL/AISP scripts/test-data.sql
@@ -0,0 +1,7 @@
+use AISP;
+
+insert into [dbo].[Login] (Id, Username, Password)
+values('1', 'Alice', 'Alice1234')
+
+insert into [dbo].[Login] (Id, Username, Password)
+values('2', 'Bob', 'Bob1234')
